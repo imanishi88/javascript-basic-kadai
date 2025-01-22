91f3b89d34a21939b0dafc865107f98f28c8578e
imanishi88 <imanishi.0810@gmail.com>
2025-01-22 16:58:44 +0900

    Update bmi.js

--- kadai-005/bmi.js
+++ kadai-005/bmi.js
@@ -5,6 +5,9 @@ bodyWeight = 68;
 let height;
 height = 1.7;
 
+let Bmi;
+Bmi = (bodyWeight)/(height*height)
+
 console.log(bodyWeight);
 console.log(height);
-console.log((bodyWeight)/(height*height));
+console.log(Bmi);
